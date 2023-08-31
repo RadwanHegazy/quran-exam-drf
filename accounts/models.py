1a38aaf70248f44bf01a8641a2406f515873d5e2
RadwanHegazy <radwangaber22@gmail.com>
2023-08-31 04:55:51 +0300

    code

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+
+@receiver(post_save, sender = User)
+def CreateToken ( created, instance, **args ) : 
+    if created : 
+        Token.objects.create( user = instance )
